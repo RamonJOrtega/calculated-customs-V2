546ae5c8b86996590a2acef6b074e551ad764185
RamonJOrtega <ramon.justis.ortega@gmail.com>
2023-05-26 16:48:25 -0700

    add cursers pointer to TodoItem.tsx

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -8,7 +8,7 @@ export function TodoItem({ id, title, complete }: TodoItemProps) {
     return (
         <li className="flex gap-1 items-center"> 
             <input id = {id} type= "checkbox" className="curser-pointer peer" />
-            <label htmlFor={id} className="peer-checked:line-through"> 
+            <label htmlFor={id} className="cursor-pointer peer-checked:line-through"> 
                 {title}  
             </label>   
         </li>   
