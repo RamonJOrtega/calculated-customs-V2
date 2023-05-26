dc9496c773abd82531b499e830677f7bd24ab010
RamonJOrtega <ramon.justis.ortega@gmail.com>
2023-05-26 16:43:27 -0700

    add TodoItem.tsx component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,31 @@
 import Link from "next/link";
 import { toEditorSettings } from "typescript";
 import { prisma } from "./db";
+import { TodoItem } from "@/components/TodoItem";
 
+function getTodos() {
+  return prisma.todo.findMany()
+}
 
 export default async function Home() {
-  const todos = await prisma.todo.findMany()
-  await prisma.todo.create({data: {title: "test", complete: false}})
+  const todos = await getTodos()
+  //await prisma.todo.create({data: {title: "test", complete: false}})
 
 
-  return <>
-  <header className="flex justify-between items-center mb-4">
-    <h1 className="text-2xl">Todos</h1>
-    <Link className="border border-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bgslate-700 outline-none"
-    href="/new"> 
-      New 
-    </Link>
-  </header>
-  <ul className="pl-4">
-    {todos.map(todo => (
-      <li key={todo.id} > {todo.title} </li>
-    ))}
-  </ul>
+  return (
+  <>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">Todos</h1>
+        <Link className="border border-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bgslate-700 outline-none"
+        href="/new"> 
+          New 
+        </Link>
+      </header>
+      <ul className="pl-4">
+        {todos.map(todo => (
+          <TodoItem key={todo.id} {...todo} />
+        ))}
+      </ul>
   </>
+  )
 } 
