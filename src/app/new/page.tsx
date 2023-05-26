a4c1864a06feb845df23b865c48a35459a37a9c9
RamonJOrtega <ramon.justis.ortega@gmail.com>
2023-05-26 16:59:05 -0700

    update page.tsx on the new page

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,3 +1,10 @@
 export default function Page() {
-    return <h1> new </h1>
+    return (
+        <>
+        <header className="flex justify-between items-center mb-4">
+            <h1 className="text-2xl">
+                New
+            </h1>
+        </header></>
+    )
 }
