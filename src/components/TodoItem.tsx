3011837c6567bdcb772e8fc0201b46f66414acdc
RamonJOrtega <ramon.justis.ortega@gmail.com>
2023-05-31 15:32:01 -0700

    make input checkbox checked when todo is complete

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -7,7 +7,7 @@ type TodoItemProps = {
 export function TodoItem({ id, title, complete }: TodoItemProps) {
     return (
         <li className="flex gap-1 items-center"> 
-            <input id = {id} type= "checkbox" className="curser-pointer peer" />
+            <input id = {id} type= "checkbox" className="curser-pointer peer" defaultChecked={complete}/>
             <label htmlFor={id} className="cursor-pointer peer-checked:line-through peer-checked:text-slate-500"> 
                 {title}  
             </label>   
