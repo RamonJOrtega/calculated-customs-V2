a6cc4b4d2e7e455fda78e0b9eede08417055177f
RamonJOrtega <ramon.justis.ortega@gmail.com>
2023-06-10 14:49:29 -0700

    move app closer to top of screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,26 +26,26 @@ export default async function Home() {
 
   return (
     <div className="h-screen flex">
-    <div className="max-w-md m-auto p-2">
+      <div className="w-screen mt-6">
          <div className="bg-neutral-900 p-6 rounded-md text-white" >
-      <header className="flex justify-between items-center mb-4">
-        <h1 className="text-2xl">Database-Driven To-Do List</h1>
-        <Link 
-        className="my-2 border border-slate-300 px-2 py-1 rounded 
-        hover:bg-slate-700 focus-within:bgslate-700 outline-none"
-        href="/new"> 
-          Add to List
-        </Link>
-      </header>
-      <ul className="pl-4">
-        {todos.map(todo => (
-          <div >
-            <TodoItem key={todo.id} {...todo} removeTodo={removeTodo} toggleTodo={toggleTodo} />
-          </div>
-        ))}
-      </ul>
-      </div>
-           </div> 
+          <header className="flex justify-between items-center mb-5">
+            <h1 className="text-2xl">Database-Driven To-Do List</h1>
+              <Link 
+              className="my-2 border border-slate-300 px-2 py-1 rounded 
+               hover:bg-slate-700 focus-within:bgslate-700 outline-none"
+              href="/new"> 
+              Add to List
+              </Link>
+          </header>
+          <ul className="pl-4">
+            {todos.map(todo => (
+              <div >
+                <TodoItem key={todo.id} {...todo} removeTodo={removeTodo} toggleTodo={toggleTodo} />
+              </div>
+            ))}
+          </ul>
         </div>
+      </div> 
+    </div>
   )
 } 
